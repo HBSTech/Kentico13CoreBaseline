973f2a37aa9d4bdd079fc3dd9348dcbef3cdd999
Trevor Fayas <tfayas@hbs.net>
2022-01-19 15:27:16 -0800

    Typo/Namespace fixes

--- MVC/MVC/Features/Sitemap/SiteMapController.cs
+++ MVC/MVC/Features/Sitemap/SiteMapController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Generic.Controllers.Administrative
+namespace Generic.Features.Sitemap
 {
     public class SiteMapController : Controller
     {
