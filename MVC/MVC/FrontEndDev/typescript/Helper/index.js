4e2cd5e46eceb7613c6e40e51c6445ae3f39cd33
Trevor Fayas <tfayas@hbs.net>
2022-06-15 11:12:08 -0500

    add to gitignore node compiled files, and deleted from repo

--- MVC/MVC/FrontEndDev/typescript/Helper/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SiteMethods_1 = require("./Helpers/SiteMethods");
-// Can access your methods with window.SiteMethods.theMethodName()
-window.SiteMethods = SiteMethods_1.SiteMethods;
-//# sourceMappingURL=index.js.map
